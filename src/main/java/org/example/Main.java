cdeef3e82c4690fc4b53750c39ae7ced5b53c0fc
carlosaegar <carlos.saegar@educa.jcyl.es>
2026-04-17 08:56:59 +0200

    Update Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,27 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    static void main() {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        IO.println(String.format("Hello and welcome!"));
+import java.sql.*;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            IO.println("i = " + i);
+public class Main {
+    public static void main(String[] args) {
+
+        String url ="jdbc:oracle:thin:@localhost:1521:xe";
+        String usuario = "RIBERA";
+        String contraseña = "ribera";
+
+        try(Connection conn = DriverManager.getConnection(url, usuario, contraseña);
+            Statement statement = conn.createStatement()){
+
+            String sql = "INSERT INTO EMPLEADO (ID, NOMBRE, SALARIO) VALUES (?, ? , ?)";
+            PreparedStatement ps  = conn.prepareStatement(sql);
+            ps.setInt(1, 10);
+            ps.setString(2, "Carlos");
+            ps.setDouble(3, 3500.0);
+            ps.executeUpdate();
+            System.out.println("Empleado insertado");
+        }catch(SQLException e ){
+            System.out.println("Error al insertar" + e.getMessage());
         }
+
     }
 }
